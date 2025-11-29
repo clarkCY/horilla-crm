851bc913e37bf1cae1ed36cf9e6f6ec540e2ccf8
Horilla <support@horilla.com>
2025-11-29 09:41:01 +0530

    [FIX] CORE: Version info page accessible without authentication

--- horilla_core/version_info.py
+++ horilla_core/version_info.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class VersionInfotemplateView(TemplateView):
+class VersionInfotemplateView(LoginRequiredMixin, TemplateView):
     template_name = "version_info/info.html"
